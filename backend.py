862dd51bf6e69db52e60d829f1d1cd31e6f47a2e
nick <nifuller@protonmail.com>
2023-03-19 13:48:29 -0700

    added error handling

--- backend.py
+++ backend.py
@@ -6,24 +6,35 @@ class Bozobot:
         openai.api_key = CHATGPT_KEY
 
     def get_response(self, user_input):
-        response = openai.Completion.create(
-            engine="text-davinci-003",
-            prompt=user_input,
-            max_tokens=4000,
-            temperature=0.5
-        ).choices[0].text
-        return response
+        try:
+            response = openai.Completion.create(
+                engine="text-davinci-003",
+                prompt=user_input,
+                max_tokens=4000,
+                temperature=0.5
+            ).choices[0].text
+            return response
+        except openai.InvalidRequestError as e:
+            message = f"Invalid Reuest: {e}"
+            return message
+        except openai.APIError as e:
+            message = f"OpenAI API returned an API Error: {e}"
+            return message
     
     def get_prompt(self, user_input):
         try:
             response = openai.Image.create(
                 prompt = user_input,
                 n=1,
-                size="1024x1024"
+                size="512x512"
             )
             image_url = response['data'][0]['url']
             return image_url
-        except openai.InvalidRequestError:
-            message = "Something went wrong!"
+        except openai.InvalidRequestError as e:
+            message = f"Invalid Request: {e}"
+            return message
+        
+        except openai.APIError as e:
+            message = f"OpenAI API returned an API Error: {e}"
             return message
  
--- main.py
+++ main.py
@@ -17,6 +17,8 @@ async def on_ready():
 
 @client.event
 async def on_message(message):
+    num_calls = 0
+    
     if message.author == client.user:
         return
 
